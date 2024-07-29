664c35d37d5fb51116d96cfda9ade4dd4d99eba0
Nicholas Campbell <nicwcampbell@gnail.com>
2024-07-29 21:02:26 +0200

    setup Redux store and userSlice which includes the async calls to the firebase auth APIs and resulting state.

---
+++ src/state/user/userSlice.js
@@ -0,0 +1,109 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { auth } from "../../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+export const login = createAsyncThunk(
+  "user/login",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return userCredential.user;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const signUp = createAsyncThunk(
+  "user/signUp",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return userCredential.user;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const signOutUser = createAsyncThunk(
+  "user/signOutUser",
+  async (_, { rejectWithValue }) => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+const initialState = {
+  user: null,
+  isLoading: true,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(signUp.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(signUp.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(signOut.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(signOut.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(signOut.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { setUser } = userSlice.actions;
+
+export default userSlice.reducer;
